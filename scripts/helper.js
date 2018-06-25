e0d2528336dc45b561673d9edb6618c2bb176de7
Parris Bryant II <Parris@owners-mbp.lan>
2018-06-25 07:14:43 -0400

    added assignment to master b/c i liked functionality

---
+++ scripts/helper.js
@@ -0,0 +1,10 @@
+class Helper {
+  playPauseAndUpdate(song){
+    player.playPause(song);
+    $('button#play-pause').attr('playState',player.playState);
+    if(!song) {return;}
+    $('.total-time').text(song.duration);
+  }
+}
+
+const helper = new Helper()
